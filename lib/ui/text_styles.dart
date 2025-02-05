1ab542c1132bfce8dc5b3db1ca1f364a8fbf9238
lcw <skcjddn@gmail.com>
2025-02-05 17:29:46 +0900

    refactor: apply Poppins font to text styles
    - Changed text styles to use the 'Poppins' font family.
    - Removed the dependency of GoogleFonts.

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -1,74 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 abstract class TextStyles {
-  static TextStyle titleTextBold = GoogleFonts.poppins(
+  static const TextStyle titleTextBold = TextStyle(
     fontSize: 50,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle headerTextBold = GoogleFonts.poppins(
+  static const TextStyle headerTextBold = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle largeTextBold = GoogleFonts.poppins(
+  static const TextStyle largeTextBold = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle mediumTextBold = GoogleFonts.poppins(
+  static const TextStyle mediumTextBold = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle normalTextBold = GoogleFonts.poppins(
+  static const TextStyle normalTextBold = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle smallTextBold = GoogleFonts.poppins(
+  static const TextStyle smallTextBold = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle smallerTextBold = GoogleFonts.poppins(
+  static const TextStyle smallerTextBold = TextStyle(
     fontSize: 11,
     fontWeight: FontWeight.bold,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle titleTextRegular = GoogleFonts.poppins(
+  static const TextStyle titleTextRegular = TextStyle(
     fontSize: 50,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle headerTextRegular = GoogleFonts.poppins(
+  static const TextStyle headerTextRegular = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle largeTextRegular = GoogleFonts.poppins(
+  static const TextStyle largeTextRegular = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle mediumTextRegular = GoogleFonts.poppins(
+  static const TextStyle mediumTextRegular = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle normalTextRegular = GoogleFonts.poppins(
+  static const TextStyle normalTextRegular = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle smallTextRegular = GoogleFonts.poppins(
+  static const TextStyle smallTextRegular = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 
-  static TextStyle smallerTextRegular = GoogleFonts.poppins(
+  static const TextStyle smallerTextRegular = TextStyle(
     fontSize: 11,
     fontWeight: FontWeight.normal,
+    fontFamily: 'Poppins',
   );
 }
