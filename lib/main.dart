703ba1fc416029ad2027d2a1a8d7ce2678ae08d4
lcw <skcjddn@gmail.com>
2025-03-03 21:27:24 +0900

    feat: add new ui components and rating dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/core/presentation/components/big_button.dart';
+import 'package:flutter_recipe_app/core/presentation/components/filter_button.dart';
 import 'package:flutter_recipe_app/core/presentation/components/input_field.dart';
+import 'package:flutter_recipe_app/core/presentation/components/rating_button.dart';
+import 'package:flutter_recipe_app/core/presentation/components/two_tab.dart';
+import 'package:flutter_recipe_app/core/presentation/dialogs/rating_dialog.dart';
+import 'package:flutter_recipe_app/core/routing/router.dart';
 import 'package:flutter_recipe_app/ui/text_styles.dart';
 
 import 'core/presentation/components/medium_button.dart';
@@ -15,13 +20,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: const ColorScheme.light(),
+        scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
     );
   }
 }
@@ -37,6 +43,37 @@ class MyHomePage extends StatelessWidget {
       ),
       body: ListView(
         children: [
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (_) {
+                  return RatingDialog(
+                      title: 'Rate recipe',
+                      score: 3,
+                      actionName: 'Send',
+                      onChange: (sceore) {
+                        print(sceore);
+                      });
+                },
+              );
+            },
+            child: Text('RatingDialog'),
+          ),
+          TwoTab(
+            labels: [
+              'label1',
+              'label2',
+            ],
+            selectedIndex: 1,
+            onChange: (int index) {
+              print('TwoTab: $index');
+            },
+          ),
+          const RatingButton('text'),
+          const RatingButton('text', isSelected: true),
+          const FilterButton('text'),
+          const FilterButton('text', isSelected: true),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: BigButton(
